65e81a3e0763c87f534f85e4e15ad4960726d9b6
Ab Qyum <qyumjahid@gmail.com>
2020-08-22 11:16:18 +0600

    Update picture.py

--- picture.py
+++ picture.py
@@ -3,3 +3,11 @@
 plt.hist(train_y,bins=50,color='green',histtype='step')
 plt.hist(test_y,bins=50,color='red',histtype='step')
 plt.show()
+
+
+#for baseline model..................
+
+train_mean_y=np.mean(train_y)
+print(train_mean_y)
+print(np.mean(np.square(test_y-train_mean_y)))
+print(np.mean(np.square(test_y- 0.0 )))
